90dc71d5e82865b6dcc055c861b315bbb3d11853
alisson-aguiars2k <alisson-aguiars2k@users.noreply.github.com>
2022-06-06 01:03:07 +0000

    deploy: a8655b6105aee4cc4df736cb572101682021394d

---
+++ precache-manifest.a7c3ffd5f23492ce5cf89346a8e4a637.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d512b6467eedd41a6acd4b859f7a728",
+    "url": "/slider/index.html"
+  },
+  {
+    "revision": "4df39c70570f25ee522e",
+    "url": "/slider/static/css/main.d3640626.chunk.css"
+  },
+  {
+    "revision": "e1c4e21724a78a9e8542",
+    "url": "/slider/static/js/2.2c3ffba9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/slider/static/js/2.2c3ffba9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4df39c70570f25ee522e",
+    "url": "/slider/static/js/main.29760a28.chunk.js"
+  },
+  {
+    "revision": "ce1caf34cf0aba241fa2",
+    "url": "/slider/static/js/runtime-main.c6fd19b8.js"
+  }
+]);
